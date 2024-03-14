e0e4589fe3886ba15060b6c6d30132586708cdcb
zptqghsw <83914995+zptqghsw@users.noreply.github.com>
2024-03-14 20:30:52 +0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://p.hohoo.fun/bare/',
+    bare: 'https://dmapi.hohoo.fun/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
